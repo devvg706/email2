85a15c86cfc47f4e3607d9cd1c3c955db94858aa
vijay ghindodey <vijayghindodey2@gmail.com>
2024-12-22 21:09:47 +0530

    sendingemail_workflowfails

--- emailtest.py
+++ emailtest.py
@@ -6,9 +6,9 @@ import os
 def send_failure_email(repository_name, workflow_name, workflow_run_id,):
    
     # Sender email credentials (customize for your SMTP server)
-    sender_email = os.getenv("sender_email") # Your email address
-    sender_password = os.getenv("reciever_email")  # Your email password (use secure storage for real applications)
-    reciever_email = os.getenv("reciever_email")
+    SENDERS_EMAIL = os.getenv("SENDERS_EMAIL") # Your email address
+    SENDERS_PASSWORD = os.getenv("SENDERS_PASSWORD")  # Your email password (use secure storage for real applications)
+    RECIEVER_EMAIL = os.getenv("RECIEVER_EMAIL")
     # Construct the email content
     subject = f"Workflow Failure: {workflow_name}"  # Email subject
     body = f"""
@@ -28,8 +28,8 @@ def send_failure_email(repository_name, workflow_name, workflow_run_id,):
 
     # Create a MIME message object for the email
     message = MIMEMultipart()  # Multi-part email to support attachments if needed
-    message["From"] = sender_email  # Specify the sender
-    message["To"] = reciever_email  # Specify the recipient
+    message["From"] = SENDERS_EMAIL  # Specify the sender
+    message["To"] = RECIEVER_EMAIL  # Specify the recipient
     message["Subject"] = subject  # Specify the subject
     message.attach(MIMEText(body, "plain"))  # Attach the email body as plain text
 
@@ -37,10 +37,10 @@ def send_failure_email(repository_name, workflow_name, workflow_run_id,):
         # Establish connection to the SMTP server
         server = smtplib.SMTP('smpt.gmail.com', 587)  # Connect to the SMTP server
         server.starttls()  # Upgrade the connection to a secure encrypted TLS/SSL connection
-        server.login(sender_email, sender_password)  # Log in to the SMTP server
+        server.login(SENDERS_EMAIL, SENDERS_PASSWORD)  # Log in to the SMTP server
 
         # Send the email
-        server.sendmail(sender_email, reciever_email, message.as_string())  # Send the email as a string
+        server.sendmail(SENDERS_EMAIL, RECIEVER_EMAIL, message.as_string())  # Send the email as a string
 
         print("Failure email sent successfully!")  # Log success
     except Exception as e:
@@ -51,3 +51,7 @@ def send_failure_email(repository_name, workflow_name, workflow_run_id,):
 
 
 send_failure_email(os.getenv('workflow_name'), os.getenv('rep_name'), os.getenv('workflow_run_id'))
+
+
+
+#in this enviornment variables are stored in os
